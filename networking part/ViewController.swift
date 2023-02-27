839362eea03892935890c98016c36613af7ae347
Vladimir Alekseev <vovauu@icloud.com>
2023-02-27 19:01:44 +0300

    Initial Commit

---
+++ networking part/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  networking part
+//
+//  Created by Вова on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
